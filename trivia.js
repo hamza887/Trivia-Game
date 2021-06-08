20bea636088b47b8137157bee4771bf58de61c34
hamza887 <hamzabutt887>
2021-06-08 14:17:50 -0400

    adding questions quite hard to find the answers and the options for these questions

--- trivia.js
+++ trivia.js
@@ -4,7 +4,49 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-//u gotta add event listener for the stuff and then you have to add the stuff in htere
+let questions = [
+    {
+    
+    question: "Who invented JavaScript?",
+    answer: "Brendan Eich",
+    options: [
+      "Douglas Crockford",
+      "Brendan Eich",
+      "Sheryl Sandberg",
+      "Bills Gates"
+    ]
+  },
+    {
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    question: "Which one of these is a JavaScript package manager?",
+    answer: "npm",
+    options: [
+      "TypeScript",
+      "Node.js",
+      "npm",
+      "ALG"
+    ]
+  },
+    {
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  };
+  //u gotta add event listener for the stuff and then you have to add the stuff in htere
 //add a function with startgame
 //add a function eith next question
 //i think we have to add the array where you have to add all the wuestions for me \
