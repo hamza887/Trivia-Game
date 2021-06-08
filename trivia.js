e173e67d814258d480839ab582221cccbbe46373
hamza887 <hamzabutt887>
2021-06-08 13:57:45 -0400

    alwasy forgto to commit

---
+++ trivia.js
@@ -0,0 +1,11 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+//u gotta add event listener for the stuff and then you have to add the stuff in htere
+//add a function with startgame
+//add a function eith next question
+//i think we have to add the array where you have to add all the wuestions for me \
+//i think have to add the questions 
