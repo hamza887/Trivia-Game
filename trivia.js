e5aaf8ffe82df27b03a4dd5d293b265f941fb2ce
hamza887 <hamzabutt887>
2021-06-10 07:30:22 -0400

    updating dom elements

--- trivia.js
+++ trivia.js
@@ -4,3 +4,16 @@
 //add a function eith next question
 //i think we have to add the array where you have to add all the wuestions for me \
 //i think have to add the questions 
+
+const question= document.querySelector('.question');
+const option1= document.querySelector('#option1'); 
+
+const option2=document.querySelector('#option2');
+const option3=document.querySelector('#option3');
+const option4=document.querySelector('#option4');
+
+const submit=document.querySelector('#submit');
+
+// console.log(question);
+// console.log(option3);
+// console.log(submit)
